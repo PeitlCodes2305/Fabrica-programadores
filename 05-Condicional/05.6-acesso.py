c4526c4789d862a3a7dd7f5ec820caaf8e175102
PeitlCodes2305 <gabriel.rodrigues.p.0509@gmail.com>
2026-06-13 11:40:53 -0300

    Add files via upload

---
+++ 05-Condicional/05.6-acesso.py
@@ -0,0 +1,15 @@
+# informaçoes de acesso
+email = input("Digite seu email: ")
+senha = input("Digite sua senha: ")
+
+# verificando o email
+if email == "teste@teste.com.br":
+    print("Email correto")
+else:
+    print("Usuário não cadastrado")
+
+# verificando a senha
+if senha == "123456":
+    print("bem vindo ao sistema da fabrica")
+else:
+    print("Senha incorreta")
